6952d3a9eb27398fcff49d50882b4685799c6389
sujith <sujithkonduru2203@gmail.com>
2026-04-25 09:02:41 +0530

    community

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ import WorkshopRegister from "./WorkshopRegister";
 import WorkshopSuccess from "./WorkshopSuccess";
 import WorkshopAlreadyRegistered from "./WorkshopAlreadyRegistered";
 import IntermediateResults from "./IntermediateResults";
-import JoinCommunity from "./joinCommunity";
+import JoinCommunity from "./JoinCommunity";
 
 
 function App() {
