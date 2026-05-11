9d6172e658c57f47ffd14625f99ad9f1d125b9f3
sujith <sujithkonduru2203@gmail.com>
2026-05-11 14:03:14 +0530

    Fix production CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,8 +25,18 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.options('*', cors());
